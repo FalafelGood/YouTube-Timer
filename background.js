71e22e89ed63716781f5a8863aadf5289bf38ade
Hudson Leone <leoneht0@gmail.com>
2025-05-31 14:49:45 -0600

    modified getCurrentTab so it returns a promise instead of requiring a callback

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 // A.M.D.G.
-let timeRemaining = 1*30*60;
+let timeRemaining = 10;
 let clockInterval = undefined;
 
 console.log("Background is live");
@@ -8,49 +8,70 @@ chrome.storage.sync.set({"timeRemaining": timeRemaining}).then(() => {
     console.log(`YouTube time is set for ${timeRemaining}s.`);
 });
 
+
 // Copied from developer.chrome.com/docs/extensions/reference/api/tabs
-function getCurrentTab(callback) {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    chrome.tabs.query(queryOptions, ([tab]) => {
-      if (chrome.runtime.lastError)
-      console.error(chrome.runtime.lastError);
-      // `tab` will either be a `tabs.Tab` instance or `undefined`.
-      callback(tab);
+// function getCurrentTab(callback) {
+//     let queryOptions = { active: true, lastFocusedWindow: true };
+//     chrome.tabs.query(queryOptions, ([tab]) => {
+//       if (chrome.runtime.lastError) console.error(chrome.runtime.lastError);
+//       // `tab` will either be a `tabs.Tab` instance or `undefined`.
+//       callback(tab);
+//     });
+// }
+
+// Style: use async-await with promises rather than callbacks wherever possible
+function getCurrentTab() {
+    return new Promise((resolve, reject) => {
+        const queryOptions = { active: true, lastFocusedWindow: true };
+        chrome.tabs.query(queryOptions, ([tab]) => {
+            if (chrome.runtime.lastError) {
+                reject(chrome.runtime.lastError);
+            } else {
+                resolve(tab);
+            }
+        });
     });
-  }
+}
 
 
 function activateClock() {
     if (!clockInterval) {
-        clockInterval = setInterval(() => {
+        clockInterval = setInterval(async () => {
             timeRemaining--;
-            chrome.storage.sync.set({"timeRemaining": timeRemaining}).then(() => {
-                /* Do nothing */
-            });
+            // Old way... Probably not good style
+            // chrome.storage.sync.set({"timeRemaining": timeRemaining}).then(() => {
+            //     /* Do nothing */
+            // });
+            await chrome.storage.sync.set({"timeRemaining": timeRemaining});
             if (timeRemaining == 0) {
                 clearInterval(clockInterval);
                 // Send a message to block current tab
-                getCurrentTab((tab) => {
-                    console.log("Getting current tab within activateClock")
-                    console.log(tab)
-                    chrome.tabs.sendMessage(tab.id, {type: "block"});
-                })
+                // getCurrentTab((tab) => {
+                //     console.log("Getting current tab within activateClock")
+                //     console.log(tab)
+                //     chrome.tabs.sendMessage(tab.id, {type: "block"});
+                // })
+                const tab = await getCurrentTab();
+                console.log("Got tab within activateClock")
+                console.log(tab)
+                chrome.tabs.sendMessage(tab.id, {type: "block"});
             };
             console.log(timeRemaining); // debug
         }, 1000);
     }
 }
 
+
 function stopClock() {
     clearInterval(clockInterval);
     clockInterval = undefined;
 }
 
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     console.log("onUpdate triggered")
     if (tab.url && tab.url.includes("youtube.com/watch")) {
         if (timeRemaining <= 0) {
-            // TODO
             chrome.tabs.sendMessage(tabId, {type: "block"});
             return;
         };
@@ -62,13 +83,14 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     }
 })
 
+
 chrome.tabs.onActivated.addListener(({ tabId }) => {
     console.log("onActivated triggered")
     console.log(tabId);
+    // Get tab corresponding to tabId
     chrome.tabs.get(tabId, (tab) => {
         if (tab.url && tab.url.includes("youtube.com/watch")) {
             if (timeRemaining <= 0) {
-                // TODO
                 chrome.tabs.sendMessage(tabId, {type: "block"});
                 return;
             };
